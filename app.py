409a82439da375cb413826717af317f1dfbff048
jaquinof08 <118316459+jaquinof08@users.noreply.github.com>
2025-07-07 20:22:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ class User(UserMixin, db.Model):
     nombre = db.Column(db.String(80), nullable=False)
     apellido = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
